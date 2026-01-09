add51edd9b031d7b69d41e858897a0e03a7179b7
Antoniya Nemtserova <antoniya@Antoniyas-MacBook-Pro.local>
2026-01-09 21:14:14 +0200

    add basic todo list

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TextInput, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [input, setInput] = useState('');
+	const [todos, setTodos] = useState([]);
+	const textChangeHandler = (text) => {
+		setInput(text);
+	};
+	const createTaskHandler = () => {
+		if (!input) return alert('No task is added.');
+		alert(input);
+		const newTodo = {
+			input,
+			isCompleted: false,
+		};
+
+		setTodos((todos) => [...todos, newTodo]);
+		setInput('');
+	};
+
+	return (
+		<View style={styles.body}>
+			<View>
+				<Text style={styles.heading}>To do list</Text>
+			</View>
+			<View style={styles.taskBox}>
+				<TextInput
+					placeholder="Add task"
+					value={input}
+					onChangeText={textChangeHandler}
+				/>
+				<Button
+					title="Add task"
+					onPress={createTaskHandler}
+					onSubmitEditing={createTaskHandler}
+				/>
+			</View>
+			<View>
+				<Text>View</Text>
+			</View>
+		</View>
+	);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = {
+	body: {
+		padding: 20,
+		alignItems: 'center',
+	},
+	heading: {
+		fontSize: 30,
+		marginTop: 30,
+		fontWeight: 'bold',
+	},
+	taskBox: {
+		flexDirection: 'row',
+		width: 300,
+		borderWidth: 1,
+		justifyContent: 'space-between',
+		borderRadius: 5,
+		marginTop: 20,
+	},
+};
